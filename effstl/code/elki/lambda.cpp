aaa71bfed6c2bcab41d9ce2d8ab86248c753f6ef
Elkiss-PC <jominhyuk87@gmail.com>
2017-04-27 02:06:50 +0900

    Update lambda example.
    - more study

--- effstl/code/elki/lambda.cpp
+++ effstl/code/elki/lambda.cpp
@@ -14,7 +14,9 @@ public:
 
 public:
     void MakeOdd() { if (_oddNum % 2 == 0) _oddNum++; }
-    void PrintMember() { std::cout << "num = " << _oddNum << std::endl; }
+    void PrintMember() const { std::cout << "num = " << _oddNum << std::endl; }
+    int GetOddNum() { return _oddNum; }
+    void AddNum(int num) { _oddNum += num; }
 private:
     int _oddNum;
 };
@@ -26,8 +28,7 @@ void main()
     // 1. using lambda function
     MyClassMap myclassMap;
 
-    for (size_t i = 0; i < 10; i++)
-    {
+    for (size_t i = 0; i < 10; i++) {
         myclassMap[i] = std::shared_ptr<MyClass>(new MyClass(i));
     }
 
@@ -39,21 +40,60 @@ void main()
     );
 
     /*
-        num = 1
-        num = 1
-        num = 3
-        num = 3
-        num = 5
-        num = 5
-        num = 7
-        num = 7
-        num = 9
-        num = 9
+    num = 1
+    num = 1
+    num = 3
+    num = 3
+    num = 5
+    num = 5
+    num = 7
+    num = 7
+    num = 9
+    num = 9
     */
 
     // 2. capture
 
+    unsigned int count = 0;
 
+    enum Mark {
+        EAST = 0,
+        WEST,
+        NORTH,
+        SOUTH,
+    };
 
+    // [capture] (parameter) -> return { statement; }
+    auto MyFunc = [&](MyClass* myClass) -> Mark {
+        count++;
+        std::cout << "Lambda Call. count is " << count << std::endl;
+        myClass->AddNum(count);
+        auto num = myClass->GetOddNum() % 4;
+        return static_cast<Mark>(num);
+    };
+
+    for (auto& it : myclassMap) {
+        std::cout << "Mark is = " << MyFunc(it.second.get()) << std::endl;
+    }
+    /*Lambda Call. count is 1
+    Mark is = 2
+    Lambda Call. count is 2
+    Mark is = 3
+    Lambda Call. count is 3
+    Mark is = 2
+    Lambda Call. count is 4
+    Mark is = 3
+    Lambda Call. count is 5
+    Mark is = 2
+    Lambda Call. count is 6
+    Mark is = 3
+    Lambda Call. count is 7
+    Mark is = 2
+    Lambda Call. count is 8
+    Mark is = 3
+    Lambda Call. count is 9
+    Mark is = 2
+    Lambda Call. count is 10
+    */
     return;
 }
